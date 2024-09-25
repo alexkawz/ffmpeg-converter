1d04ae2a1659ed6189006a76c2d9fb5338859a5e
Alex K. <alexkaw94@gmail.com>
2024-09-24 22:54:09 -0700

    Made the script work
    Im bad

--- ffmeg-converter.sh
+++ ffmeg-converter.sh
@@ -1,6 +1,6 @@
-#ffmpeg m4a to mp3 Script
+#!/bin/bash
 
-#IN PROGRESS DO NOT USE
+#ffmpeg m4a to mp3 Script
 
 Echo "
    ___                 _    _ _ _             
@@ -16,11 +16,10 @@ Echo "
 "
 
 #Select what filetype you want to convert"
-read -p "
-What file type are you trying to convert
-1.M4a 2.WAV
-" source-file
-
+echo "What file type are you trying to convert?"
+echo "1. M4A"
+echo "2. WAV"
+read -p "Enter m4a or wav (no caps): " source_file
 
 #Prompt the user for the input filename
 read -p "Enter in the input filename: " input_file
@@ -28,11 +27,32 @@ read -p "Enter in the input filename: " input_file
 #Prompt the user for the output file name
 read -p "Enter in the output filename: " output_file
 
+#M4a
+if [ "$source_file" = "m4a" ]; then
+	Echo "
 
-if [ $source-file -eq 1 ]; then
+   ____                          _   _                   
+  / ___|___  _ ____   _____ _ __| |_(_)_ __   __ _       
+ | |   / _ \|  _ \ \ / / _ \  __| __| |  _ \ / _  |      
+ | |__| (_) | | | \ V /  __/ |  | |_| | | | | (_| |_ _ _ 
+  \____\___/|_| |_|\_/ \___|_|   \__|_|_| |_|\__, (_|_|_)
+                                             |___/       
+"
 	ffmpeg -i "$input_file".m4a -acodec libmp3lame -ab 320k "$output_file".mp3
-elif [ $source-file -eq 2 ]; then
+
+elif [ "$source_file" = "wav" ]; then
+	Echo "
+
+   ____                          _   _                   
+  / ___|___  _ ____   _____ _ __| |_(_)_ __   __ _       
+ | |   / _ \|  _ \ \ / / _ \  __| __| |  _ \ / _  |      
+ | |__| (_) | | | \ V /  __/ |  | |_| | | | | (_| |_ _ _ 
+  \____\___/|_| |_|\_/ \___|_|   \__|_|_| |_|\__, (_|_|_)
+                                             |___/       
+
+"
 	ffmpeg -i "$input_file".wav -vn -ar 44100 -ac 2 -b:a 192k "$output_file".mp3
+
 else
-	echo "Please enter 1 or 2"
+	echo "Invalid Option. Please enter m4a or wav"
 fi
